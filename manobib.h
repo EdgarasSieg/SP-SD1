ca0e074ee301d83704284535344b6440e03006fb
EdgarasSieg <edgaras.sieg@gmail.com>
2024-10-23 23:22:39 +0200

    Create manobib.h

---
+++ manobib.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <numeric>
+#include <algorithm>
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::ifstream;
+using std::ofstream;
+using std::string;
+using std::vector;
+using std::istringstream;
+using std::setw;
+using std::left;
+using std::right;
+using std::fixed;
+using std::setprecision;
